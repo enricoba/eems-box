bb5c16ef23399b3595d84d9897140a7cbc0d7978
Henrik Baran <henrik.baran@online.de>
2016-08-08 21:02:23 +0200

    monitoring

--- monitoring.py
+++ monitoring.py
@@ -1,6 +1,22 @@
 import time
 
 
-while True:
+def main():
     print 'monitoring on'
-    time.sleep(1)
+
+    # get interval from DB
+    interval = 2
+
+    timestamp = int(time.time() / interval) * interval
+    timestamp += interval
+    time.sleep(timestamp - time.time())
+
+    while True:
+        # call for values
+        print 'monitoring every 2 seconds'
+        timestamp += interval
+        time.sleep(timestamp - time.time())
+
+
+if __name__ == '__main__':
+    main()
