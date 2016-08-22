4f6ed556cbbffd85c4698b30a3ae38ddf6849563
Aurofree Hoehn <a.hoehn@mailbox.org>
2016-08-22 18:19:05 +0200

    change logger variablenames

--- logger.py
+++ logger.py
@@ -1,16 +1,6 @@
 from messagebus import Bus
 import logging
 
-
-# VARIABLES
-# STR
-# con_str_01: log format
-# con_str_02: log date format
-# con_str_03: log file
-
-# DICT
-# con_dic_01: Log-level information
-
 # Message to Log:
 # level: DEBUG, INFO, WARNING, ERROR, CRITICAL
 # source: str() z.B. Display-Job, GSM-Job, Core-Job, ...
@@ -21,21 +11,21 @@ import logging
 class Logger(object):
     def __init__(self):
         # Define Logger
-        con_str_01 = '[*] %(asctime)s %(levelname)s %(message)s'
-        con_str_02 = '%Y-%m-%d %H:%M:%S'
-        con_str_03 = 'eems.log'
+        log_format = '[*] %(asctime)s %(levelname)s %(message)s'
+        log_date_format = '%Y-%m-%d %H:%M:%S'
+        log_file = 'eems.log'
 
         logging.basicConfig(level=logging.INFO,
-                            format=con_str_01,
-                            datefmt=con_str_02,
-                            filename=con_str_03)
+                            format=log_format,
+                            datefmt=log_date_format,
+                            filename=log_file)
 
         self.logger = logging.getLogger('eems')
-        self.con_dic_01 = {'DEBUG': 10,
-                           'INFO': 20,
-                           'WARNING': 30,
-                           'ERROR': 40,
-                           'CRITICAL': 50}
+        self.log_lvl_info = {'DEBUG': 10,
+                             'INFO': 20,
+                             'WARNING': 30,
+                             'ERROR': 40,
+                             'CRITICAL': 50}
 
     def write_log(self, msg_as_dict):
         """Public function *write_log* writes messages to the log file.
@@ -43,7 +33,7 @@ class Logger(object):
         :param msg_as_dict: *dict*
         :return: *None*
         """
-        self.logger.log(self.con_dic_01[msg_as_dict[u'level']],
+        self.logger.log(self.log_lvl_info[msg_as_dict[u'level']],
                         '{}: {}'.format(msg_as_dict[u'source'],
                                         msg_as_dict[u'msg']))
 
